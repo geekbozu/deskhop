29c24fed3cf907eeccf989e81980e2fabb74dad7
Timothy Keller <geekboy1011@gmail.com>
2025-03-09 18:27:30 -0400

    Wrong place.

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -88,12 +88,12 @@ bool tud_mouse_report(uint8_t mode, uint8_t buttons, int16_t x, int16_t y, int8_
 // array of pointer to string descriptors
 char const *string_desc_arr[] = {
     (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "Hrvoje Cavrak",            // 1: Manufacturer
-    "DeskHop Switch",           // 2: Product
+    "HC",            // 1: Manufacturer
+    "DH KB/M",           // 2: Product
     "0",                        // 3: Serials, should use chip ID
-    "DeskHop Helper",           // 4: Mouse Helper Interface
-    "DeskHop Config",           // 5: Vendor Interface
-    "DeskHop Disk",             // 6: Disk Interface
+    "DH Helper",           // 4: Mouse Helper Interface
+    "DH Config",           // 5: Vendor Interface
+    "DH Disk",             // 6: Disk Interface
 #ifdef DH_DEBUG
     "DeskHop Debug",            // 7: Debug Interface
 #endif
